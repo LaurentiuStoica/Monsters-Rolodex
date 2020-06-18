586e551de5252b8a7afa3af2b58a941e3a16fbf3
Laurentiu Stoica <stoica363@gmail.com>
2020-06-18 17:35:42 +0300

    Updates

---
+++ precache-manifest.aee12abd01996d1f94c61ebe2c977dbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ded48a4f29c56d742a620d343b5d923c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "138f846c7088c7f7c793",
+    "url": "/monsters-rolodex/static/css/main.b34ab48e.chunk.css"
+  },
+  {
+    "revision": "724ff5f7fbcfae15cd6c",
+    "url": "/monsters-rolodex/static/js/2.1df26df3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.1df26df3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "138f846c7088c7f7c793",
+    "url": "/monsters-rolodex/static/js/main.6f033ad0.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
